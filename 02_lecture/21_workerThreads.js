4657ed5299b12c2a47ba4b0774c7fad0bfaa8a0c
MinhyukWoo <minhyukwoo.dev@gmail.com>
2022-10-02 14:52:48 +0900

    Update Worker's codes

--- 02_lecture/21_workerThreads.js
+++ 02_lecture/21_workerThreads.js
@@ -3,13 +3,13 @@ const { Worker, isMainThread, parentPort } = require("worker_threads");
 if (isMainThread) {
   const worker = new Worker(__filename);
   worker.on("message", (message) => {
-    console.log("from worker", message);
+    console.log("from worker", message);  // from parent ping
   });
-  worker.on("exit", () => console.log("worker exit"));
+  worker.on("exit", () => console.log("worker exit"));  // worker exit
   worker.postMessage("ping");
 } else {
   parentPort.on("message", (value) => {
-    console.log("from parent", value);
+    console.log("from parent", value);  // from worker pong
     parentPort.postMessage("pong");
     parentPort.close();
   });
